4b23e32c014e81a372a95afa6a8ce1eae1250aae
Arber <arber.mjeku@outlook.com>
2020-07-25 22:24:28 +0200

    Updates

---
+++ precache-manifest.c6ad29343ea9e9c488ce0516cd8a136b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791953eb21773189747730ce03ab0990",
+    "url": "/https//Arbermjeku.github.io/Monsters-app/index.html"
+  },
+  {
+    "revision": "dae170e076b781076543",
+    "url": "/https//Arbermjeku.github.io/Monsters-app/static/css/main.afe4eba4.chunk.css"
+  },
+  {
+    "revision": "7b91e1254827e7d2e6f5",
+    "url": "/https//Arbermjeku.github.io/Monsters-app/static/js/2.c7adede6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//Arbermjeku.github.io/Monsters-app/static/js/2.c7adede6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae170e076b781076543",
+    "url": "/https//Arbermjeku.github.io/Monsters-app/static/js/main.4ee8b846.chunk.js"
+  },
+  {
+    "revision": "4a3b915370b40edf2f81",
+    "url": "/https//Arbermjeku.github.io/Monsters-app/static/js/runtime-main.ae659dba.js"
+  }
+]);
