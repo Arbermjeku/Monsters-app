6be9bfa16488d967226fa2a4a04e1e6f7c8258f0
Arber <arber.mjeku@outlook.com>
2020-07-25 22:04:21 +0200

    commiting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React from "react";
+
+import CardList from "./components/card-list/CardList";
+import SearchBox from "./components/search-box/SearchBox";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => this.setState({ monsters: data }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    return (
+      <div className='App'>
+        <h1
+          style={{
+            fontFamily: "Bigelow Rules",
+            fontSize: "72px",
+            color: "#0ccac4",
+            textAlign: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Monsters Rolodex
+        </h1>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <SearchBox
+            placeholder="Search Monsters"
+            handleChange={this.handleChange}
+          />
+        </div>
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
